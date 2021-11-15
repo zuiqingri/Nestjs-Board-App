92fc0142086fe15d51669585fe0a2bc2a3537a45
zuiqingri <72846494+zuiqingri@users.noreply.github.com>
2021-11-15 22:28:43 +0800

    Update Board

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,5 +1,5 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
-import { Board } from './board.model';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Board, BoardStatus } from './board.model';
 import { BoardsService } from './boards.service';
 import { CreateBoardDto } from './dto/create-board.dto';
 
@@ -26,6 +26,15 @@ export class BoardsController {
 
         @Delete('/:id')
         deleteBoard(@Param('id') id:string):void{
-            this.boardsService.deleteBoard(id);        }
+            this.boardsService.deleteBoard(id)
+        }
+
+        @Patch('/:id/status')
+        updateBoardStatus(
+            @Param('id') id:string,
+            @Body('status') status:BoardStatus,
+        ){
+            return this.boardsService.updateBoardStatus(id,status)
+        }
     
 }
