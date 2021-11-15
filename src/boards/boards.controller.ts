698627460716bcdb0fe608ab31984d00e937fd1a
zuiqingri <72846494+zuiqingri@users.noreply.github.com>
2021-11-15 21:32:43 +0800

    Create Dto

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { Board } from './board.model';
 import { BoardsService } from './boards.service';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Controller('boards')
 export class BoardsController {
@@ -13,10 +14,9 @@ export class BoardsController {
 
         @Post()
         createBoard(
-            @Body('title') title:string,
-            @Body('description') description:string
+            @Body() createBoardDto:CreateBoardDto,
         ):Board{
-            return this.boardsService.createBoard(title,description);
+            return this.boardsService.createBoard(createBoardDto);
         }
     
 }
