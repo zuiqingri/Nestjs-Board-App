76064d29ee08953ba0fcdb73e21258dd17dacdf6
zuiqingri <72846494+zuiqingri@users.noreply.github.com>
2021-11-15 21:06:49 +0800

    Create Board(Service,Controller)

---
+++ src/boards/boards.controller.ts
@@ -0,0 +1,22 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Board } from './board.model';
+import { BoardsService } from './boards.service';
+
+@Controller('boards')
+export class BoardsController {
+    constructor(private boardsService:BoardsService){}
+
+        @Get()
+        getAllBoard():Board[]{
+        return this.boardsService.getAllBoards();
+        }
+
+        @Post()
+        createBoard(
+            @Body('title') title:string,
+            @Body('description') description:string
+        ):Board{
+            return this.boardsService.createBoard(title,description);
+        }
+    
+}
