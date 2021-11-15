81414c5c2550baf9e5886f0a448e9849ac583e70
zuiqingri <72846494+zuiqingri@users.noreply.github.com>
2021-11-15 22:20:12 +0800

    Read,Delete Board

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { Board } from './board.model';
 import { BoardsService } from './boards.service';
 import { CreateBoardDto } from './dto/create-board.dto';
@@ -18,5 +18,14 @@ export class BoardsController {
         ):Board{
             return this.boardsService.createBoard(createBoardDto);
         }
+
+        @Get('/:id')
+        getBoardById(@Param('id') id:string):Board{
+            return this.boardsService.getBoardById(id);
+        }
+
+        @Delete('/:id')
+        deleteBoard(@Param('id') id:string):void{
+            this.boardsService.deleteBoard(id);        }
     
 }
