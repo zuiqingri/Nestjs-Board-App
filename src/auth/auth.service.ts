2086d8c517c7cea31cd8c1557c951e1ab5e3cd95
zuiqingri <72846494+zuiqingri@users.noreply.github.com>
2021-11-16 15:56:52 +0800

    Create Token using JWT

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,23 +3,28 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
 import { UserRepository } from './user.repository';
 import * as bcrypt from 'bcryptjs'
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
     constructor(
         @InjectRepository(UserRepository)
         private userRepository:UserRepository,
+        private jwtService:JwtService
     ){}
 
     async signUp(authCredentialsDto:AuthCredentialsDto):Promise<void>{
         return this.userRepository.createUser(authCredentialsDto);
     }
 
-    async signIn(authCredentialsDto:AuthCredentialsDto):Promise<string>{
+    async signIn(authCredentialsDto:AuthCredentialsDto):Promise<{accessToken:string}>{
         const {username,password}=authCredentialsDto;
         const user=await this.userRepository.findOne({username});
         if(user && (await bcrypt.compare(password,user.password))){
-            return 'login success';
+            //Create User token(Secret + Payload)
+            const payload={username};
+            const accessToken=await this.jwtService.sign(payload);
+            return {accessToken};
         } else{
             throw new UnauthorizedException('login failed')
         }
